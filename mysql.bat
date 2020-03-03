9961a0d6bc7e11e7b01f1b14a3dcd9cc8b8917ae
Aditya Vikram <adityavikram2016@gmail.com>
2020-03-03 13:18:06 +0530

    Update mysql.bat

--- mysql.bat
+++ mysql.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Program Files\MySQL\MySQL Server 8.0\bin
 set /p dta=<commands.txt
-echo %dta% | mysql.exe -u root -pyour_password_here_PS_dont_put_spaces_between_it_and_'-p'
+cd C:\Program Files\MySQL\MySQL Server 8.0\bin
+echo %dta% | mysql.exe -u root -pdps
 pause
